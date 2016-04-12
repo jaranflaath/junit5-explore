31f99954dc0582d6e277967a67257f0f1028edc3
Jaran Flaath <jaran.flaath@gmail.com>
2016-04-12 22:08:17 +0200

    Testing new assertions

---
+++ src/main/java/no/kodehuset/junit5/Person.java
@@ -0,0 +1,17 @@
+package no.kodehuset.junit5;
+
+public class Person {
+
+    public final String name;
+
+    public final Integer age;
+
+    public final Boolean isMale;
+
+    public Person(String name, Integer age, Boolean isMale) {
+        this.name = name;
+        this.age = age;
+        this.isMale = isMale;
+    }
+
+}
--- src/test/java/no/kodehuset/junit5/JUnit5Test.java
+++ src/test/java/no/kodehuset/junit5/JUnit5Test.java
@@ -2,13 +2,49 @@ package no.kodehuset.junit5;
 
 import org.junit.gen5.api.Test;
 
-import static org.junit.gen5.api.Assertions.assertEquals;
+import static org.junit.gen5.api.Assertions.*;
 
 public class JUnit5Test {
 
     @Test
-    void aSimpleTest() {
+    void equalsTest() {
 
         assertEquals(1, 1);
     }
+
+
+    @Test
+    void assertAllTest() {
+
+        Person person = new Person("Jaran Flaath", 34, true);
+
+        assertAll(
+                () -> assertEquals("Jaran Flaath", person.name),
+                () -> assertEquals(34, person.age),
+                () -> assertEquals(true, person.isMale)
+        );
+    }
+
+
+    @Test
+    void assertThrowsTest() {
+
+        assertThrows(IllegalArgumentException.class, this::throwIllegalArgumentException);
+
+    }
+
+    @Test
+    void expectThrowsTest() {
+
+        IllegalArgumentException ex = expectThrows(IllegalArgumentException.class, this::throwIllegalArgumentException);
+        assertEquals("Fo'sho!", ex.getMessage());
+    }
+
+
+    private void throwIllegalArgumentException() {
+
+        throw new IllegalArgumentException("Fo'sho!");
+
+    }
+
 }
